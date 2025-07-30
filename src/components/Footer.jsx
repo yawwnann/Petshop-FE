df9e20d7cfb752202bae500ce97c20044ac6857d
yawwnann <c0478592@gmail.com>
2025-07-30 11:51:01 +0700

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -56,31 +56,7 @@ const Footer = () => {
             </ul>
           </div>
 
-          {/* Contact Info */}
-          <div>
-            <h4 className="text-lg font-semibold mb-4 text-white">Kontak</h4>
-            <ul className="space-y-3">
-              <li className="flex items-start space-x-3">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mt-0.5 text-white/80 flex-shrink-0" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-                <span className="text-white/80">Jl. Siluk Panggang, Siuk 1, Selopamioro, Kec. Imogiri, Kabupaten Bantul, Daerah Istimewa Yogyakarta 55782</span>
-              </li>
-              <li className="flex items-center space-x-3">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white/80" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-                </svg>
-                <a href="tel:+6282134346802" className="text-white/80 hover:text-white transition-colors">+62 821-3434-6802</a>
-              </li>
-              <li className="flex items-center space-x-3">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white/80" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-                </svg>
-                <a href="mailto:buanapetshop@gmail.com" className="text-white/80 hover:text-white transition-colors">buanapetshop@gmail.com</a>
-              </li>
-            </ul>
-          </div>
+        
 
           {/* Business Hours */}
           <div>
